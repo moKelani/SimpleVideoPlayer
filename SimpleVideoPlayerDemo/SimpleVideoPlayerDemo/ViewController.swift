ef30be3fb731b116d7b74cb28f9a14a6122a3ec3
Mohamed El-Said <m.elsaid@eightytech.com>
2020-02-21 15:42:22 +0300

    Create Project

---
+++ SimpleVideoPlayerDemo/SimpleVideoPlayerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleVideoPlayerDemo
+//
+//  Created by Mohamed El-Said on 2/21/20.
+//  Copyright © 2020 Mohamed El-Said. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
